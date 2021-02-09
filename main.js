52171ddafa014ce6492c1de1da876ae378da8ec3
anndrill <72471008+anndrill@users.noreply.github.com>
2021-02-09 19:26:27 +0300

    homework
    add homework files

---
+++ main.js
@@ -0,0 +1,39 @@
+// Задание 1.
+
+// let a = 1;
+// let b = 100;
+// while (a < b) {
+//     a++;
+//     for (let c = 2; c < a; c++) {
+//         if (a % c != 0) {
+//             console.log(a);
+//         }
+//         break;
+//     }
+// }
+
+//Задание 2 и 3. 
+
+// x = [10, 4, 35, 8, 73, 60];
+// let countBasketPrice = x.reduce(function (acc, elem) {
+//     return acc + elem;
+// }, 0)
+// console.log(countBasketPrice);
+
+// Задание 4.
+
+// for (let x = 0; x <= 9; x++)
+//     console.log(x); {
+
+// }
+
+// Задание 5.
+
+let x = [];
+let y = 0;
+while (y < 20) {
+    y++;
+    x.push('x');
+    console.log(x);
+}
+
